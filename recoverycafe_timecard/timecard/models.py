14f98f92da097be98c5a2628bf62a1f1c8adcf40
Andrew Beyer <beyer.andrew@gmail.com>
2016-10-15 15:24:13 -0700

    schema changes

--- recoverycafe_timecard/timecard/models.py
+++ recoverycafe_timecard/timecard/models.py
@@ -5,13 +5,23 @@ from django.db import models
 class Branch(models.Model):
     branch_name = models.CharField(max_length=64)
 
+    def __unicode__(self):
+        return u'Branch(%s)' % self.branch_name
+
+
 class Task(models.Model):
     task_name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return u'Task(%s)' % self.task_name
 
 class Volunteer(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     staff_id = models.IntegerField(unique=True)
+
+    def __unicode__(self):
+        return u'User(%s %s)' % (self.first_name, self.last_name)
 
 class PunchTime(models.Model):
     volunteer_id = models.ForeignKey(
@@ -27,7 +37,7 @@ class PunchTime(models.Model):
             on_delete=models.CASCADE
     )
     punch_type = models.CharField(
-            max_length=64,
+            max_length=8,
             choices = (
                 ('IN', 'Punch In'),
                 ('OUT', 'Punch Out'),
@@ -36,3 +46,6 @@ class PunchTime(models.Model):
     punch_time = models.DateTimeField()
     flags = models.IntegerField()
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'PunchTime(%s %s, %s, %s)' % (self.volunteer_id.first_name, self.volunteer_id.last_name, self.get_punch_type_display(), unicode(self.punch_time))
