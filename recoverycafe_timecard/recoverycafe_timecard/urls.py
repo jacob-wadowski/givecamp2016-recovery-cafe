834d504d80906f71036a9df866ee1d37fa6a5cc3
Josh <josh@halfnibble.com>
2016-10-15 22:14:46 -0700

    Add default home page view.

--- recoverycafe_timecard/recoverycafe_timecard/urls.py
+++ recoverycafe_timecard/recoverycafe_timecard/urls.py
@@ -19,7 +19,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 
+from . import views
+
 urlpatterns = [
+    url(r'^$', views.HomePage.as_view(), name='homepage'),
     url(r'^login/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^timecard/', include('timecard.urls')),
