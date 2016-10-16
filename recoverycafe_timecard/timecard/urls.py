83f4809265fefc89d88dac0fc90fbb28e4930754
K <tachk@rocketeeringetc.net>
2016-10-15 19:30:13 -0700

    Adding routing for upcoming staff admin pages.

--- recoverycafe_timecard/timecard/urls.py
+++ recoverycafe_timecard/timecard/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.render_volunteer_page, name='render_volunteer_page'),
-    url(r'^admin/volunteers/', views.render_admin_volunteers_page, name='render_admin_volunteers_page')
+    url(r'^admin/volunteers/', views.render_admin_volunteers_page, name='render_admin_volunteers_page'),
+    url(r'^admin/tasks/', views.render_admin_tasks_page, name='render_admin_tasks_page'),
+    url(r'^admin/reports/', views.render_admin_reports_page, name='render_admin_reports_page'),
 ]
