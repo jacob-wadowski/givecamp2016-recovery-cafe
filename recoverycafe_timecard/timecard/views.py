6b2ed6aed26a4f7130790bc415cd99621cfc4931
K <tachk@rocketeeringetc.net>
2016-10-15 20:33:49 -0700

    Adding in routing and view changes (missed from last change).

--- recoverycafe_timecard/timecard/views.py
+++ recoverycafe_timecard/timecard/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+from django.shortcuts import render
 
 import json
 
-# Create your views here.
-
-from .forms import PostForm
 from .models import Task, PunchTime
+
 
 DB_CURSOR = connection.cursor()
 
@@ -21,9 +20,25 @@ def dict_fetchall(cursor):
     ]
 
 
-def render_volunteer_page(request):
-    return render(request, 'volunteer.html')
-
 def receive_json(request):
     data = {"name": "Jacob Wadowski", "event": "Check In"}
     return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+def render_volunteer_page(request):
+    return render(request, 'volunteer.html')
+
+
+def render_admin_volunteers_page(request):
+    queryset_volunteers = DB_CURSOR.execute('SELECT * FROM view_last_known_status')
+    return render(request, 'admin_volunteers.html', {'volunteer_list': dict_fetchall(queryset_volunteers)})
+
+
+def render_admin_tasks_page(request):
+    queryset_tasks = Task.objects.all()  # List of tasks
+    return render(request, 'admin_tasks.html', {'task_list': queryset_tasks})
+
+
+def render_admin_reports_page(request):
+    queryset_reports = PunchTime.objects.all()  # One workbook w/multiple sheets
+    return render(request, 'admin_reports.html', {'reports': queryset_reports})
