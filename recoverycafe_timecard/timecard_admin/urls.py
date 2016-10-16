a439a9c0d6ed3374e0d714b9bd583ee2f623de2f
K <tachk@rocketeeringetc.net>
2016-10-15 20:56:40 -0700

    Porting the model integration to the correct app (timecard_admin).

--- recoverycafe_timecard/timecard_admin/urls.py
+++ recoverycafe_timecard/timecard_admin/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.render_admin_page, name='render_admin_page'),
+    url(r'^admin/volunteers/', views.render_admin_volunteers_page, name='render_admin_volunteers_page'),
+    url(r'^admin/tasks/', views.render_admin_tasks_page, name='render_admin_tasks_page'),
+    url(r'^admin/reports/', views.render_admin_reports_page, name='render_admin_reports_page'),
 ]
