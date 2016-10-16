464f9bba48c0bdc70356914ca518fab06e5d8310
Andrew Beyer <beyer.andrew@gmail.com>
2016-10-15 19:02:35 -0700

    pull base template and static files to top level

--- recoverycafe_timecard/recoverycafe_timecard/settings.py
+++ recoverycafe_timecard/recoverycafe_timecard/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'recoverycafe_timecard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../timecard/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,8 @@ USE_TZ = True
 
 # URL prefix for static files.
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
