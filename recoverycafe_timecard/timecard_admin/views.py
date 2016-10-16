d93f7c65c024290b5a03af216e1869cc9460a5d0
Josh <josh@halfnibble.com>
2016-10-16 14:54:56 -0700

    Create export view.

--- recoverycafe_timecard/timecard_admin/views.py
+++ recoverycafe_timecard/timecard_admin/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse, QueryDict, HttpResponseRedirect
 from django.db import connection, transaction
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
 
 import json
 from timecard.models import LastKnownStatus, PunchTime, Task, Volunteer
-from utils.import_volunteers import get_volunteer_records
+from .utils.import_volunteers import get_volunteer_records
+from .utils.export_data import get_report_data
 
 
 def render_admin_page(request):
@@ -53,3 +55,34 @@ def import_volunteers(request):
                     staff_id=r['staff_id'], defaults=r)
 
     return HttpResponseRedirect(reverse(render_admin_page))
+
+
+class ReportView(generic.View):
+    """
+    Generate XLXS file downloads based off of POST parameters.
+    """
+    def post(self, request, *args, **kwargs):
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        return self.generate_response(start_date, end_date)
+
+    def get(self, request, *args, **kwargs):
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+
+        return self.generate_response(start_date, end_date)
+
+    def generate_response(self, start_date, end_date):
+        data = get_report_data(start_date, end_date)
+
+        # Generate XLSX file from data
+        xlsx = None # <<<<< SET to XLSX
+
+        # ... Magic is done. Return response.
+        attachment = 'attachment; filename="Report Data.xlsx"'
+        response = HttpResponse(xlsx, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = attachment
+        return response
+        
+
