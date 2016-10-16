f950ddaf57b0b91e42384f378f8486e9885bc76c
K <tachk@rocketeeringetc.net>
2016-10-15 19:15:26 -0700

    Adding model integration and initial routing for staff admin ''volunteers' page.

--- recoverycafe_timecard/timecard/urls.py
+++ recoverycafe_timecard/timecard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.render_volunteer_page, name='render_volunteer_page'),
+    url(r'^admin/volunteers/', views.render_admin_volunteers_page, name='render_admin_volunteers_page')
 ]
--- recoverycafe_timecard/timecard/views.py
+++ recoverycafe_timecard/timecard/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 from .forms import PostForm
+from .models import Volunteer
 
 def render_volunteer_page(request):
     return render(request, 'volunteer.html')
+
+def render_admin_volunteers_page(request):
+    queryset_volunteers = Volunteer.objects.all()
+    return render(request, 'admin_volunteers.html', {'volunteer_list': queryset_volunteers})
