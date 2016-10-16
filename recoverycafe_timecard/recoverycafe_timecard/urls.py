ecf75dbce248ca4ed98db6ec3d7614ebc12d2e60
jc <juancarlos.hm@outlook.com>
2016-10-15 20:32:06 -0700

    fixed urls after rebase

--- recoverycafe_timecard/recoverycafe_timecard/urls.py
+++ recoverycafe_timecard/recoverycafe_timecard/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^login/', include('login.urls')),
