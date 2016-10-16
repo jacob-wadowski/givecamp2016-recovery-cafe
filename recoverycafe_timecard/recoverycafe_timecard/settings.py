963f6e5c96aa60650191e938bc6cda369c5e2a99
jc <juancarlos.hm@outlook.com>
2016-10-15 20:10:54 -0700

    login screen beta

--- recoverycafe_timecard/recoverycafe_timecard/settings.py
+++ recoverycafe_timecard/recoverycafe_timecard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
     'timecard.apps.TimecardConfig',
     'timecard_admin.apps.TimecardAdminConfig',
     'django.contrib.admin',
