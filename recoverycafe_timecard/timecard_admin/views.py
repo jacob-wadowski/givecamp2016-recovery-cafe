ed49c81fd201a18771347f61f1097a0ecbdd39b8
K <tachk@rocketeeringetc.net>
2016-10-16 01:54:32 -0700

    Updating adminView view to use the new model for the view_last_known_status sql view (instead of directly querying on the fly).

--- recoverycafe_timecard/timecard_admin/views.py
+++ recoverycafe_timecard/timecard_admin/views.py
@@ -2,28 +2,22 @@ from django.http import HttpResponse
 from django.db import connection
 from django.shortcuts import render
 
-from timecard.models import Task, PunchTime
+from timecard.models import LastKnownStatus, PunchTime, Task
 
 DB_CURSOR = connection.cursor()
 
 
-def dict_fetchall(cursor):
-    "Return all rows from a cursor as a dict"
-    columns = [col[0] for col in cursor.description]
-    return [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
-
 def render_admin_page(request):
-    queryset_volunteers = DB_CURSOR.execute('SELECT * FROM view_last_known_status')
+    queryset_volunteers = LastKnownStatus.objects.filter(punch_type_latest='IN')
     queryset_tasks = Task.objects.all()  # List of tasks
     queryset_reports = PunchTime.objects.all()  # One workbook w/multiple sheets
-    return render(request, 'admin.htm', {'volunteer_list': dict_fetchall(queryset_volunteers), 'task_list': queryset_tasks, 'reports': queryset_reports})
+    return render(request, 'admin.htm', {'volunteer_list': queryset_volunteers, 'task_list': queryset_tasks, 'reports': queryset_reports})
+
 
 def render_admin_volunteers_page(request):
-    queryset_volunteers = DB_CURSOR.execute('SELECT * FROM view_last_known_status')
-    return render(request, 'admin_volunteers.html', {'volunteer_list': dict_fetchall(queryset_volunteers)})
+    queryset_volunteers = LastKnownStatus.objects.all()
+    return render(request, 'admin_volunteers.html', {'volunteer_list': queryset_volunteers})
+
 
 def render_admin_tasks_page(request):
     queryset_tasks = Task.objects.all()  # List of tasks
